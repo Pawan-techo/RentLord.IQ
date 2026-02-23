3d86e929ab5ca853de7405c88bc0ddcffafb18c0
Pawan-techo <pawanbhuyar333@gmail.com>
2026-02-23 13:06:55 +0530

    path change in landlord dashboard

--- client/src/components/Landlord/DashbordLayout.jsx
+++ client/src/components/Landlord/DashbordLayout.jsx
@@ -1,7 +1,7 @@
 import { Menu } from "lucide-react";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "./SideBar";
+import Sidebar from "./Sidebar";
 const DashboardLayout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
